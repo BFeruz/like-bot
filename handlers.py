ad6fc55356df0e71489fb7c042d73a609cac64db
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-18 16:01:14 +0500

    like

--- handlers.py
+++ handlers.py
@@ -5,6 +5,7 @@ from db import (
     is_user,
     add_user,
     get_user,
+    inc_like,
 )
 
 
@@ -22,22 +23,32 @@ def start(update: Update, context: CallbackContext):
             text=f'Press one of the buttons.',
             reply_markup=inline_keyboard
         )
+    else:
+        user_data = get_user(chat_id=str(user.id))
+
+        update.message.reply_html(
+            text=f'<b>likes:</b> {user_data["likes"]}\n<b>dislikes:</b> {user_data["dislikes"]}',
+            reply_markup=keyboard
+        )
+        update.message.reply_html(
+            text=f'<b>inline likes:</b> {user_data["inline_likes"]}\n<b>inline dislikes:</b> {user_data["inline_dislikes"]}',
+            reply_markup=inline_keyboard
+        )
+
+def like(update: Update, context: CallbackContext):
+    user = update.effective_user
+
+    if not is_user(str(user.id)):
+        start(update, context)
+        return
+
+    inc_like(chat_id=str(user.id))
 
     user_data = get_user(chat_id=str(user.id))
-
     update.message.reply_html(
         text=f'<b>likes:</b> {user_data["likes"]}\n<b>dislikes:</b> {user_data["dislikes"]}',
         reply_markup=keyboard
     )
-    update.message.reply_html(
-        text=f'<b>inline likes:</b> {user_data["inline_likes"]}\n<b>inline dislikes:</b> {user_data["inline_dislikes"]}',
-        reply_markup=inline_keyboard
-    )
-
-def like(update: Update, context: CallbackContext):
-
-    update.message.reply_html(text=f'likes: {LIKES}\ndislikes: {DISLIKES}')
-
 
 def dislike(update: Update, context: CallbackContext):
 
